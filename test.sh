863b0eb0d5ec9c68861ce42e3f8972dd136e8af6
random2907 <yranjeetyadav1234567890@gmail.com>
2024-03-26 20:43:34 +0530

    add calender generator

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+
+
+if [ $# -gt 0 ]; then
+	x=$(cat $1 | jq -r '.[] | .title, .start, .end')
+else
+	x=$(cat - | jq -r '.[] | .title, .start, .end')
+fi
+
+echo -e "BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN" > nice.ics
+
+xcount=$(echo "$x" | wc -l)
+
+for (( i=1; i<=$xcount; i+=3 )); do
+	echo "BEGIN:VEVENT" >> nice.ics
+	echo "SUMMARY:$(echo "$x" | sed -n "$i"p)" >> nice.ics
+	echo "DTSTART;TZID=Asia/Kolkata:$(date -d "$(echo "$x" | sed -n "$((i+1))"p)" +%Y%m%dT%H%M%S)" >> nice.ics
+	echo "DTEND;TZID=Asia/Kolkata:$(date -d "$(echo "$x" | sed -n "$((i+2))"p)" +%Y%m%dT%H%M%S)" >> nice.ics
+	echo -e "SEQUENCE:3\nBEGIN:VALARM\nTRIGGER:-PT10M\nACTION:DISPLAY\nEND:VALARM\nEND:VEVENT" >> nice.ics
+done
+
+echo "END:VCALENDAR" >> nice.ics
+
